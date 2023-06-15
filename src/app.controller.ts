34560b3dd4765f002d82b52297de9ebfe8e5b6b1
Matheus <ymatheus.h@gmail.com>
2023-06-15 04:04:06 -0300

    feat: :sparkles: implements create member with validation body

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateTeamMemberBody } from './dtos/create-team-member-body';
+import { RocketMembersRepository } from './repositories/rocket-members-repository';
 
-@Controller()
+@Controller('app')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private rocketMembersRepository: RocketMembersRepository) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('hello')
+  async getHello(@Body() body: CreateTeamMemberBody) {
+    const { name, function: memberFunction } = body;
+
+    await this.rocketMembersRepository.create(name, memberFunction);
   }
 }
